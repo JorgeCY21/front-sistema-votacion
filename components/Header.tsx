22d109c55d16631f10572f3cbe163ae84dda6ad9
Jorge <jcondorios@unsa.edu.pe>
2025-06-01 22:09:24 -0500

    solucionando imports

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { VoteIcon, ShieldCheckIcon } from 'lucide-react'
 export function Header() {
   return (
--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { LoginForm } from './LoginForm'
 import { Header } from './Header'
 export function LoginPage() {
